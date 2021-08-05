40dde4c9ea92b279317f25c87f76a6101d32058a
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-08-05 15:00:05 +0000

    Refactorings

--- src/auth.js
+++ src/auth.js
@@ -3,4 +3,8 @@ import store from './store';
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-    
+        store.commit('user/USER_DETAILS', user);
+    } else {
+        store.commit('user/LOGOUT')
+    }
+})
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer padless>
+    <v-card flat tile class="orange lighten-1 white--text text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" ic
