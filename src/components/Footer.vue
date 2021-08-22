1fa331fa176acb3613c4b8f840522cf043af644f
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-08-22 09:27:00 +0000

    .m

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,4 +2,13 @@
   <v-footer padless>
     <v-card flat tile class="orange lighten-1 white--text text-center">
       <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" ic
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+        Mauris cursus commodo interdum. Praesent ut ris
