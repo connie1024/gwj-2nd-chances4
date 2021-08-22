75fdd0ee9de4b1855e22dd5dff686db445a9b2ba
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-08-22 18:21:51 +0000

    Initial repository

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,4 +15,12 @@
         accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a
         sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
         lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-        iaculis dui sollicitudin.
+        iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor
+        vel ut orci. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <st
