4a3037192605a4bc047c733c3a395bff5f59ccc5
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-09-19 16:43:18 +0000

    net, scm: fix PaX detected msg_controllen overflow in scm_detach_fds

--- src/components/HomePage/Search.vue
+++ src/components/HomePage/Search.vue
@@ -1,3 +1,17 @@
 <template>
   <div style="margin-top: 50px">
-    Searching for '{{ this.searchterm }}
+    Searching for '{{ this.searchterm }}'
+    <v-tabs
+      v-model="tab"
+      background-color="orange accent-4"
+      centered
+      dark
+      icons-and-text
+    >
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab v-on:click="fetchItems('sale')">
+        Sale/Trade
+      </v-tab>
+
+      <v-tab v-on:click="fetchItems('rent')">
+        Rental
