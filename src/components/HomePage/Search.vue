53c2098bf45805e022d1103bdfbf28ae6bf19209
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-10-16 15:08:56 +0000

    license change

--- src/components/HomePage/Search.vue
+++ src/components/HomePage/Search.vue
@@ -46,4 +46,15 @@
               align="center"
               class="mx-0"
               style="margin-bottom: 10px; padding-left: 20px;"
-  
+            >
+              <v-rating
+                v-bind:value="x[2]"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+              ></v-rating>
+
+              <div class="grey--text ml-4">{{ x[2] }} ({{ x[4] }} reviews)</div>
+     
