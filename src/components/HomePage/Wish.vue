74093f9f654e23200afccbe8f6b43c24feac54cd
connie1024 <99495227+connie1024@users.noreply.github.com>
2021-12-28 14:10:18 +0000

    Update summary.md

--- src/components/HomePage/Wish.vue
+++ src/components/HomePage/Wish.vue
@@ -7,4 +7,12 @@
           <v-col
             v-for="(x, i) in items"
             v-bind:key="i"
-   
+            col="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card :loading="false" class="mx-auto my-12" max-width="374">
+              <v-card-actions>
+                <v-list-item class="grow" style="font-weight: 700">
+                  <v-list
