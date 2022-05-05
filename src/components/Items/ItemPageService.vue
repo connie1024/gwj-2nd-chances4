6bff83d2140f49fa15cd15c27a138bd526788635
connie1024 <99495227+connie1024@users.noreply.github.com>
2022-05-05 08:05:36 +0000

    README Update

--- src/components/Items/ItemPageService.vue
+++ src/components/Items/ItemPageService.vue
@@ -6,4 +6,12 @@
       {{ x[1].Title }} for {{ x[1].Type }}
     </h1>
     <v-container>
- 
+      <v-row no-gutters>
+        <v-col v-for="(x, i) in itemInfo" v-bind:key="i" md="12">
+          <v-card color="grey lighten-4">
+            <v-card-text>
+              <v-row no-gutters>
+                <v-col md="6">
+                  <v-card color="grey lighten-4">
+                    <v-carousel>
+           
