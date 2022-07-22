0aabb50fa751553f4c5cb304d2848ade350c5bdf
connie1024 <99495227+connie1024@users.noreply.github.com>
2022-07-22 05:49:25 +0000

    Update handout.md

--- src/components/UserPage/CfmDone.vue
+++ src/components/UserPage/CfmDone.vue
@@ -62,4 +62,16 @@ export default {
       this.dialog = true;
       this.title = title;
       this.message = message;
-      this.options = Obj
+      this.options = Object.assign(this.options, options);
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject = reject;
+      });
+    },
+    agree() {
+      this.resolve(true);
+      this.dialog = false;
+    },
+    cancel() {
+      this.resolve(false);
+      
