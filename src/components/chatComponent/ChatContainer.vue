7746bcc1b7a42c7b671db028b568d21053364c4c
connie1024 <99495227+connie1024@users.noreply.github.com>
2022-08-06 05:33:47 +0000

    more modifications

--- src/components/chatComponent/ChatContainer.vue
+++ src/components/chatComponent/ChatContainer.vue
@@ -1,2 +1,13 @@
 <template>
   <div class="window-container" :class="{ 'window-mobile': isDevice }">
+    <chat-window
+      :height="screenHeight"
+      :theme="theme"
+      :styles="styles"
+      :current-user-id="currentUserId"
+      :room-id="roomId"
+      :rooms="loadedRooms"
+      :loading-rooms="loadingRooms"
+      :messages="messages"
+      :messages-loaded="messagesLoaded"
+      :rooms-loaded
