2f2584f2f77be677543b2c201d7686c37d511023
connie1024 <99495227+connie1024@users.noreply.github.com>
2023-03-19 11:40:49 +0000

    content tweaks

--- src/components/chatComponent/ChatContainer.vue
+++ src/components/chatComponent/ChatContainer.vue
@@ -739,4 +739,10 @@ export default {
       this.fetchRooms();
     },
 
-    async delete
+    async deleteRoom(roomId) {
+      const room = this.rooms.find((r) => r.roomId === roomId);
+      if (
+        room.users.find((user) => user._id === "SGmFnBZB4xxMv9V4CVlW") ||
+        room.users.find((user) => user._id === "6jMsIXUrBHBj7o2cRlau")
+      ) {
+        return alert("Nope, for demo purposes you cannot delete this
